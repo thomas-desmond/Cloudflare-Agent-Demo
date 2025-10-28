bea9997f7b4b3604845ecf2cc82722f048a61b50
Thomas Desmond <24610108+thomas-desmond@users.noreply.github.com>
2025-10-28 15:36:44 -0700

    Change text to be more specific to each agent.

--- agents/basic-math-agent/src/app.tsx
+++ agents/basic-math-agent/src/app.tsx
@@ -159,7 +159,7 @@ export default function Chat() {
           </div>
 
           <div className="flex-1">
-            <h2 className="font-semibold text-base">AI Chat Agent</h2>
+            <h2 className="font-semibold text-base"> Basic Math Agent</h2>
           </div>
 
           <div className="flex items-center gap-2 mr-2">
